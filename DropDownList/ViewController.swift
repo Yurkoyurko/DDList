2e46a91cc857dd42251decd4467c006173dcdb1d
Elon Musk <ElonMusk@elons-mac-mini.local>
2018-01-20 11:48:52 +0200

    Initial Commit

---
+++ DropDownList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DropDownList
+//
+//  Created by Elon Musk on 1/20/18.
+//  Copyright © 2018 Yuriy Babych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
